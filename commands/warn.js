772cd9dd222deb30434a8d99d90b691a2b396bc1
majesticmilky <64874607+majesticmilky@users.noreply.github.com>
2020-10-31 12:45:25 -0700

    Bug Fix 2

--- commands/warn.js
+++ commands/warn.js
@@ -5,6 +5,9 @@ module.exports = {
     description: 'warn command',
     execute(message, args){
         const { prefix, token } = require('../config.json');
+        const reason = args.slice(2).join(" ");
+        const member = message.mentions.members.first();
+
         const warnEmbed = new Discord.MessageEmbed()
             .setColor('#ffea61')
             .setDescription('You don\'t have permission');
@@ -12,14 +15,18 @@ module.exports = {
             const warnEmbed2 = new Discord.MessageEmbed()
             .setColor('#ffea61')
             .setDescription('You need to tag someone');
+            const warnEmbed4 = new Discord.MessageEmbed()
+            .setColor('#ffea61')
+            .setDescription('You need a reason');
 
             const warnEmbed3 = new Discord.MessageEmbed()
             .setColor('#ffea61')
-            .setDescription(' ' + args[1] +  ' was warned by ' + message.member.user.tag);
+            .setDescription(' ' + args[1] +  ' was warned by <@' + message.member.user.id + '> for ' + reason);
 
             if(!message.member.permissions.has("MANAGE_CHANNELS")) return message.channel.send(warnEmbed);
 
             if(!args[1])return message.channel.send(warnEmbed2);
+            if(!args[2])return message.channel.send(warnEmbed4);
 
             message.channel.send(warnEmbed3);
     }
