df5083d8d2934b846dc2f6d6de416f7afe8b2bb5
majesticmilky <64874607+majesticmilky@users.noreply.github.com>
2020-10-31 12:47:42 -0700

    Bug Fix 6

--- commands/ban.js
+++ commands/ban.js
@@ -4,7 +4,10 @@ module.exports = {
     name: 'ban',
     description: 'ban command',
     execute(message, args){
+        const member = message.mentions.members.first();
         const { prefix, token } = require('../config.json');
+        const reason = args.slice(2).join(" ");
+
         const banEmbed = new Discord.MessageEmbed()
             .setColor('#ffea61')
             .setDescription('You don\'t have permission');
@@ -12,12 +15,16 @@ module.exports = {
             const banEmbed2 = new Discord.MessageEmbed()
             .setColor('#ffea61')
             .setDescription('You need to tag someone');
+            const warnEmbed2 = new Discord.MessageEmbed()
+            .setColor('#ffea61')
+            .setDescription('You need to reason');
 
             const banEmbed3 = new Discord.MessageEmbed()
             .setColor('#ffea61')
-            .setDescription(' ' + args[1] + ' was banned by ' + message.member.user.tag);
+            .setDescription(' ' + args[1] + ' was banned by <@' + message.author.id + '> for ' + reason);
 
             if(!args[1])return message.channel.send(banEmbed2);
+            if(!args[2])return message.channel.send(warnEmbed2);
 
             if(message.member.permissions.has("BAN_MEMBERS")){
                member.ban().then((member) => {
